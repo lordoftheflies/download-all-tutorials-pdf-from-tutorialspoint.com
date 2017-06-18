fdda6fed75ac4ed999022620b5eca1054be26044
Vipin Joshi <vipinjoshi1999@gmail.com>
2017-06-18 15:30:45 +0530

    Update for Scraper.py
    Added exception handling for the errors occured while scraping tutorials with no PDF available.

--- scraper.py
+++ scraper.py
@@ -13,9 +13,13 @@ for ul in ull:
         print "Working Directory - %s"%cd
         links = ul.findAll('a')
         for link in links:
-                sub = link['href'].split("/")[1]
-                fname = sub+"_tutorial.pdf"
-                flink = "http://tutorialspoint.com/"+sub+"/"+fname
-                print "Processing Link - %s"%flink
-                u(flink,fname)
+                try:
+                        sub = link['href'].split("/")[1]
+                        fname = sub+"_tutorial.pdf"
+                        flink = "http://tutorialspoint.com/"+sub+"/"+fname
+                        print "Processing Link - %s"%flink
+                        u(flink,fname)
+                except Exception, e:
+                        print "[!!] err - No PDF for "+str(link['href'])
+                        continue
         os.chdir("..")
